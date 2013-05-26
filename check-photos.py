eed36a0e8d7a0b5c7b9ddb329a85a3d195df6dc4
Gustaf Sterner <gsterner@gmail.com>
2013-05-26 22:00:26 +0200

    added more models

--- check-photos.py
+++ check-photos.py
@@ -3,7 +3,7 @@ import pyexiv2
 
 photo_stream_path = "C:\\Users\\Gustaf\\Pictures\\Photo Stream\\My Photo Stream\\"
 photo_stream_list = os.listdir(photo_stream_path)
-
+model_names = ['iPhone 4', 'iPhone 5', 'iPhone 3GS', 'iPad']
 #photo_name = 'IMG_2882.JPG'
 
 for photo_name in photo_stream_list:
@@ -11,8 +11,8 @@ for photo_name in photo_stream_list:
 	metadata.read()
 	if 'Exif.Image.Model' in metadata:
 		model = metadata['Exif.Image.Model']
-		if model.value != 'iPhone 4':
-			print 'model', model
+		if model.value not in model_names:
+			print 'model', model, photo_name
 		
 	else :
 		print 'no model'
