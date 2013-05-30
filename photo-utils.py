4ba25b3dc37f4052e2955eb03144ef3814dce87b
Gustaf Sterner <gsterner@gmail.com>
2013-05-30 21:57:22 +0200

    Introduced utility file with functionality for copying files

---
+++ photo-utils.py
@@ -0,0 +1,16 @@
+import shutil
+import os
+
+__author__ = 'Gustaf'
+
+SOURCE_DIR = "C:\\Users\\Gustaf\\Documents\\photo-test\\source"
+DESTINATION_DIR = "C:\\Users\\Gustaf\\Documents\\photo-test\\destination"
+SLASH = '\\'
+
+def copy_photo(source_dir, dest_dir, file_name):
+    file_path_source = source_dir + SLASH + file_name
+    file_path_dest = dest_dir + SLASH + file_name
+    shutil.copyfile(file_path_source, file_path_dest)
+
+for photo_name in os.listdir(SOURCE_DIR):
+    copy_photo(SOURCE_DIR, DESTINATION_DIR, photo_name)
