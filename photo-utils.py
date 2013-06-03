4a24f8518bff2cc935101304260cf8c312a628da
Gustaf Sterner <gsterner@gmail.com>
2013-06-03 20:43:41 +0200

    utility for joining paths

--- photo-utils.py
+++ photo-utils.py
@@ -7,10 +7,12 @@ SOURCE_DIR = "C:\\Users\\Gustaf\\Documents\\photo-test\\source"
 DESTINATION_DIR = "C:\\Users\\Gustaf\\Documents\\photo-test\\destination"
 SLASH = '\\'
 
+
 def copy_photo(source_dir, dest_dir, file_name):
     file_path_source = source_dir + SLASH + file_name
     file_path_dest = dest_dir + SLASH + file_name
     shutil.copyfile(file_path_source, file_path_dest)
 
-for photo_name in os.listdir(SOURCE_DIR):
-    copy_photo(SOURCE_DIR, DESTINATION_DIR, photo_name)
+
+def join_directory_file_name(directory, file_name):
+    return directory + SLASH + file_name
